a05531b9150b51561f180abf22cf41fbf935469b
Exellin <carnegieshawn@yahoo.ca>
2016-12-28 21:14:02 +0000

    install devise and users

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+  end
+
+  factory :admin, class: User do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    admin true
+  end
+end
+
