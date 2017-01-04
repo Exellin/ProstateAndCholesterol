1c55c00bbfae5f6aac60769bd00ed941b8ee7984
Exellin <carnegieshawn@yahoo.ca>
2017-01-04 07:02:09 +0000

    added navbar, sign in and out specs and links

---
+++ spec/features/users/sign_out_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature "Users sign out" do
+  
+  scenario do
+    @user = FactoryGirl.create(:user)
+    login_as(@user)
+    visit "/"
+    click_link("Sign out")
+    expect(page).to have_content("Signed out successfully")
+  end
+end
