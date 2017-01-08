b35f9c9d6dbf0a6a76f211ef09cce4a4c03b75f4
Exellin <carnegieshawn@yahoo.ca>
2017-01-08 01:30:09 +0000

    created profiles_controller with show view redirected to on sign up

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  
+  def empty?
+    ignored_attrs = {'id' => 1 , 'user_id' => 1, 'created_at' => 1, 'updated_at' => 1}
+    self.attributes.all?{|key, value| value.blank? || ignored_attrs[key]}
+  end
 end
---
+++ spec/features/profiles/create_profile_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "Profile created upon signup" do
+  
+  scenario do
+    @user = FactoryGirl.build(:user)
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: @user.password
+    fill_in "Password Confirmation", with: @user.password
+    click_button "Sign up"
+    
+    user = User.last
+    expect(page).to have_content("Fill in your profile")
+    profile = Profile.last
+    expect(current_path).to eq(profile_path(profile))
+    expect(profile.user_id).to eq(user.id)
+  end
+end
