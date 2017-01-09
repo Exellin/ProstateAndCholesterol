a34af90f69a6ea8b58ed78fcd9bfa9513f89b79a
Exellin <carnegieshawn@yahoo.ca>
2017-01-09 23:20:46 +0000

    added missing has_many between profile and medications

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,7 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_many :psa_histories, dependent: :destroy
   has_many :cholesterol_histories, dependent: :destroy
+  has_many :medications, dependent: :destroy
   
   def empty?
     ignored_attrs = {'id' => 1 , 'user_id' => 1, 'created_at' => 1, 'updated_at' => 1}
