8e611e8ae0edf4e50f09ec85cd37f3cacfd53962
Exellin <carnegieshawn@yahoo.ca>
2017-01-15 02:07:36 +0000

    add editing spec and button for profile

---
+++ spec/features/profiles/edit_profile_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "Editing Profle" do
+  before do
+    @owner = FactoryGirl.create(:user)
+    @profile = FactoryGirl.create(:profile, user: @owner)
+    login_as(@owner)
+  end
+  
+  scenario "with valid inputs" do
+    visit "/"
+    click_link(@owner.username)
+    click_link "edit"
+    expect(page)
+    fill_in "First Name", with: "Joe"
+    old_first_name = @profile.first_name
+    click_button "Update Profile"
+    expect(page).to have_content("Joe")
+    expect(page).not_to have_content(old_first_name)
+  end
+end
+    
+    
+    
