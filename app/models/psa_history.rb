b784c50edd03f4feaa654032828608860fec3fb5
Exellin <carnegieshawn@yahoo.ca>
2017-01-25 20:35:05 +0000

    limited psa to a number with 3 digits and a decimal

--- app/models/psa_history.rb
+++ app/models/psa_history.rb
@@ -2,5 +2,5 @@ class PsaHistory < ApplicationRecord
   belongs_to :profile
   validates :year, presence: true
   validates :month, presence: true
-  validates :psa, presence: true
+  validates :psa, presence: true, :inclusion => { :in => 0..999.9, :message => "must be between 0 and 999.9" }
 end
