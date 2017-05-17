ad3aa05f979d59915bad28962a0c069b8be4e6d9
Exellin <carnegieshawn@yahoo.ca>
2017-05-17 19:41:04 +0000

    add create_psa_history_spec

--- app/controllers/psa_histories_controller.rb
+++ app/controllers/psa_histories_controller.rb
@@ -1,6 +1,7 @@
 class PsaHistoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_profile
+  before_action :require_same_user
     
   def index
     if @profile.psa_histories.nil?
@@ -13,5 +14,12 @@ class PsaHistoriesController < ApplicationController
   
   def set_profile
     @profile = Profile.find(params[:profile_id])
+  end
+  
+  def require_same_user
+    if current_user != @profile.user and !current_user.admin?
+      flash[:danger] = "You can only edit or delete your own profile"
+      redirect_to root_path
+    end
   end
 end
