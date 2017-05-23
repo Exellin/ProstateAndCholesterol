a5ed3e0b99ab31310a80eefa321c3c1a0cd8367d
Exellin <carnegieshawn@yahoo.ca>
2017-05-23 23:01:32 +0000

    add list_posts_spec feature spec

---
+++ spec/features/posts/list_posts_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.feature "Listing Posts" do
+  before do
+    @user = FactoryGirl.create(:user)
+    @topic = FactoryGirl.create(:topic)
+    @posts_list = FactoryGirl.create_list(:post, 31, topic: @topic, user: @user)
+    @first_post = @posts_list[0]
+    @last_post = @posts_list[-1]
+    @last_post_comment = FactoryGirl.create(:comment, post: @last_post, user: @user)
+    visit "/"
+    click_link "Forum"
+    click_link @topic.name
+  end
+
+  scenario "shows 30 posts per page" do
+    expect(page).to have_content(@last_post.title)
+    expect(page).not_to have_content(@first_post.title)
+    click_link "2"
+    expect(page).not_to have_content(@last_post.title)
+    expect(page).to have_content(@first_post.title)
+  end
+  
+  scenario "has a toggle to show content for each post", js: true do
+    post_content_toggle = find(:css, "#expand_post_#{@last_post.id}")
+    post_content = find(:css, "content_for_post_#{@last_post.id}")
+    post_content_toggle.click
+    post_content.should be_visible
+    post_content_toggle.click
+    post_content.should_not be_visible
+  end
+    
+  scenario "shows basic information for each post" do
+    expect(page).to have_content(@topic.name)
+    expect(page).to have_content("0 comments")
+    expect(page).to have_content("1 comment")
+    expect(page).to have_content("created less than a minute ago by #{@user.username}")
+  end
+end
+    
