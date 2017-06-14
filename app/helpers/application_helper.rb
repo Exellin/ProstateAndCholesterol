22923d298d2751504c61ee9b6b83bc8ba7ca0bd8
Exellin <carnegieshawn@yahoo.ca>
2017-06-14 01:01:24 +0000

    add months_select_array helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,9 @@ module ApplicationHelper
       else name
     end
   end
+  
+  def months_select_array
+    [["January", "1"], ["February", "2"], ["March", "3"], ["April", "4",], ["May", "5"], ["June", "6"], 
+    ["July", "7"], ["August", "8"], ["September", "9"], ["October", "10"], ["November", "11"], ["December", "12"]]
+  end
 end
