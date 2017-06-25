a7233d69beb43a6f8d46157abd7fef7e2a5fa201
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-06-25 15:02:09 -0600

    remove form-control margin bottom on error

--- db/schema.rb
+++ db/schema.rb
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20170513160906) do
     t.integer  "age_bladder_infection"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "psa_histories", force: :cascade do |t|
