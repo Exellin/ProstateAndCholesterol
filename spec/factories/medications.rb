ea3af89bc78e7988342a993dc7db32eb1a3618f4
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-06-26 13:11:47 -0600

    add selects to medication year and age fields and update table css

--- spec/factories/medications.rb
+++ spec/factories/medications.rb
@@ -4,8 +4,8 @@ FactoryGirl.define do
   factory :medication do
     association :profile, factory: :profile
     purpose { ["Prostate", "Cholesterol", "Blood Pressure"].sample }
-    age_recommended { Faker::Date.between(120.years.ago, 18.years.ago).year }
-    age_prescribed { Faker::Date.between(120.years.ago, 18.years.ago).year }
+    age_recommended { Faker::Number.between(18, 120) }
+    age_prescribed { Faker::Number.between(18, 120) }
     name { Faker::Lorem.word }
     strength { Faker::Number.between(1, 2000) }
     dosage { Faker::Number.between(1, 10) }
--- spec/features/medications/create_medication_spec.rb
+++ spec/features/medications/create_medication_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "Creating Medication" do
     @profile = FactoryGirl.create(:profile, user: @owner)
     @medication = FactoryGirl.build(:medication, profile: @profile)
   end
-  
+
   feature "as the owner", js: true do
     before do
       login_as(@owner)
@@ -16,15 +16,16 @@ RSpec.feature "Creating Medication" do
       click_link "edit medications"
       click_link "Add an entry"
     end
-    
+
     scenario "with valid inputs" do
       find("select[name$='[purpose]']").select(@medication.purpose)
       find("input[name$='[name]']").set(@medication.name)
       find("input[name$='[strength]']").set(@medication.strength)
       find("input[name$='[dosage]']").set(@medication.dosage)
-      find("input[name$='[age_recommended]']").set(@medication.age_recommended)
+      find("select[name$='[age_recommended]']").select(@medication.age_recommended)
+      find("select[name$='[age_prescribed]']").select(@medication.age_prescribed)
       find("select[name$='[month_last_used]']").select(@medication.month_last_used)
-      find("input[name$='[year_last_used]']").set(@medication.year_last_used)
+      find("select[name$='[year_last_used]']").select(@medication.year_last_used)
       find("input[name$='[still_using]']").set(@medication.still_using)
       click_button "Update Medications"
       expect(page).to have_content("Your Medications have been successfully updated")
@@ -33,9 +34,10 @@ RSpec.feature "Creating Medication" do
       expect(find("input[name$='[name]']").value).to eq @medication.name
       expect(find("input[name$='[strength]']").value).to eq @medication.strength.to_s
       expect(find("input[name$='[dosage]']").value).to eq @medication.dosage.to_s
-      expect(find("input[name$='[age_recommended]']").value).to eq @medication.age_recommended.to_s
+      expect(find("select[name$='[age_recommended]']").find('option[selected]').text).to eq @medication.age_recommended.to_s
+      expect(find("select[name$='[age_prescribed]']").find('option[selected]').text).to eq @medication.age_prescribed.to_s
       expect(find("select[name$='[month_last_used]']").find('option[selected]').text).to eq @medication.month_last_used
-      expect(find("input[name$='[year_last_used]']").value).to eq @medication.year_last_used.to_s
+      expect(find("select[name$='[year_last_used]']").find('option[selected]').text).to eq @medication.year_last_used.to_s
       if @medication.still_using
         expect(find("input[name$='[still_using]']")).to be_checked
       else
@@ -43,30 +45,30 @@ RSpec.feature "Creating Medication" do
       end
     end
   end
-  
+
   feature "as another user" do
     before do
       login_as(@user)
     end
-    
+
     scenario "through the user interface" do
       visit "/profiles/#{@profile.id}"
       expect(page).not_to have_content("edit medications")
     end
-    
+
     scenario "by going directly to the route" do
       visit "/profiles/#{@profile.id}/medications/"
       expect(page).to have_content("You can only edit or delete your own content")
       expect(current_path).to eq(root_path)
     end
   end
-  
+
   feature "as a guest" do
     scenario "through the user interface" do
       visit "/profiles/#{@profile.id}"
       expect(page).not_to have_content("edit medications")
     end
-    
+
     scenario "by going directly to the route" do
       visit "/profiles/#{@profile.id}/medications/"
       expect(page).to have_content("You must sign in or sign up to view this page")
