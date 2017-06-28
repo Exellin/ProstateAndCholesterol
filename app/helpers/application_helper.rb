562bd8ebdc9f4953533b20adc1abbbf208c3d9a3
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-06-28 14:39:54 -0600

    update month select array for use with moment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,8 @@ module ApplicationHelper
   end
 
   def months_select_array
-    [["January", "1"], ["February", "2"], ["March", "3"], ["April", "4",], ["May", "5"], ["June", "6"],
-    ["July", "7"], ["August", "8"], ["September", "9"], ["October", "10"], ["November", "11"], ["December", "12"]]
+    [["January", "0"], ["February", "1"], ["March", "2"], ["April", "3",], ["May", "4"], ["June", "5"],
+    ["July", "6"], ["August", "7"], ["September", "8"], ["October", "9"], ["November", "10"], ["December", "11"]]
   end
 
   def birth_year_select_array
