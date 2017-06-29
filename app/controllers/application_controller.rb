1a4b70fb9b99b7a901bed2a98e2faaa4440d4cfe
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-06-29 01:46:12 -0600

    add redirect on sign in to profile if not empty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   private
+
+  def after_sign_in_path_for(resource)
+    if resource.profile.empty?
+      redirect_path = root_path
+    else
+      redirect_path = profile_path(resource.profile)
+    end
+  end
+
   def authenticate_user!
     if user_signed_in?
       super
